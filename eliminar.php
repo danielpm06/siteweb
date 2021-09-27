9573b92a35de7a701750ed941c82faef28e118b7
danielpm06 <91445587+danielpm06@users.noreply.github.com>
2021-09-26 22:35:29 -0500

    Add files via upload

---
+++ eliminar.php
@@ -0,0 +1,11 @@
+<?php
+include("cn.php");
+$id = $_GET['id'];
+$eliminar = "DELETE FROM usuarios WHERE id='$id'";
+$resultadoEliminar=mysqli_query($conexion,$eliminar);
+
+if($resultadoEliminar){
+	
+	header("Location:mostrartabla.php");
+}
+	
