bc40c0e7e0a4a11651a2c71a12a13196873f4b31
danielpm06 <91445587+danielpm06@users.noreply.github.com>
2021-09-26 23:16:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function insertDate(){
+function insertDate() {
     var nick = document.getElementById("nick").value;
     if ( nick == "" ){ 
        return alert('Ingresa el login que utilizas en el servidor de argentina limando carter.');
