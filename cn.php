6c29f7105df55c142ac9e854c232b33d7ece472d
danielpm06 <91445587+danielpm06@users.noreply.github.com>
2021-09-26 22:44:15 -0500

    Update cn.php

--- cn.php
+++ cn.php
@@ -1,4 +1,4 @@
 <?php
 
-$conexion = mysqli_connect("localhost","root","","bd");
+$conexion = mysqli_connect("na2.ultra-h.com","server_6846","12345","server_6846_players");
 mysqli_set_charset($conexion,"utf8");
