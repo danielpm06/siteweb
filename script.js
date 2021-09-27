bb310bee0b6eb19e70af1d9ba4d716899f59202a
danielpm06 <91445587+danielpm06@users.noreply.github.com>
2021-09-26 23:45:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,4 +8,31 @@ function insertDate() {
     if (login == "") {
        return alert('Ingresa el Login que utilizas en el servidor de argentina limando carter.');
     }
+    
+    var mysql  = require('mysql');
+    var connection = mysql.createConnection({
+        host     : 'na2.ultra-h.com',
+        user     : 'server_6846',
+        password : '12345',
+        port: '3306',
+        database: 'server_6846_players',
+        useConnectionPooling:true
+    });
+    
+    connection.connect((err) => {
+      if (err) {
+        console.log("Database Connection Failed !!!", err);
+      } else {
+        console.log("connected to Database");
+      }
+    });
+  
+    
+    client.query({
+      name: 'insert beatle',
+      text: "INSERT INTO players (USER,LOGIN,DAYS) values($1, $2, $3)",
+      values: [nick, login, 30]
+    });
+
+    
 }
