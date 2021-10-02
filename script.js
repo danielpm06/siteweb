bd94666087847642c5f139b197b8c1bbbc19cd80
danielpm06 <91445587+danielpm06@users.noreply.github.com>
2021-10-01 21:48:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,31 +9,7 @@ function insertDate() {
     if (login == "") {
        return
     }
-    
-    var mysql  = require('mysql');
-    var connection = mysql.createConnection({
-        host     : 'na2.ultra-h.com',
-        user     : 'server_6846',
-        password : '12345',
-        port: '3306',
-        database: 'server_6846_players',
-        useConnectionPooling:true
-    });
-    
-    connection.connect((err) => {
-      if (err) {
-        console.log("Database Connection Failed !!!", err);
-      } else {
-        console.log("connected to Database");
-      }
-    });
-  
-    
-    client.query({
-      name: 'insert beatle',
-      text: "INSERT INTO players (USER,LOGIN,DAYS) values($1, $2, $3)",
-      values: [nick, login, 30]
-    });
+    window.alert("Hi")
 
     
 }
