acbb5184aa60876fea62f702d8c8313ada4069d3
danielpm06 <91445587+danielpm06@users.noreply.github.com>
2021-10-01 22:13:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 
 function insertDate() {
-    var nick = document.getElementById("login").value;
-    var login = document.getElementById("pass").value;   
+    var login = document.getElementById("login").value;
+    var pass = document.getElementById("pass").value;   
     
-    if ( nick == "" ){ 
+    if ( login == "" ){ 
        return swal("Algo anda mal!", "No puedes dejar esté campo vacio!", "error");
     } 
-    if (login == "") {
+    if (pass == "") {
        return swal("Algo anda mal!", "No puedes dejar esté campo vacio!", "error");
     } 
+    //instanciamos el objetoAjax
+    ajax=objetoAjax();
+    //uso del medotod POST
+    //archivo que realizará la operacion
+    //registro.php
+    ajax.open("POST", "registro.php",true);
+    ajax.onreadystatechange=function() {
+        if (ajax.readyState==4) {
+        //mostrar resultados en esta capa
+        divResultado.innerHTML = ajax.responseText
+        //llamar a funcion para limpiar los inputs
+        LimpiarCampos();
+        }
+    }
+    ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+    //enviando los valores
+    ajax.send("logIn="+login+"&Pass="+pass)
 }
+
+function LimpiarCampos(){
+  document.getElementById("login").value="";
+  document.getElementById("pass").value="";
+ }
