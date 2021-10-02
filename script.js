22402a9d4aa0b3418aea7513c4c78b70c8d88050
danielpm06 <91445587+danielpm06@users.noreply.github.com>
2021-10-01 22:19:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -28,6 +28,23 @@ function insertDate() {
     ajax.send("logIn="+login+"&Pass="+pass)
 }
 
+function objetoAjax(){
+var xmlhttp=false;
+try {
+xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+} catch (e) {
+try {
+xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+} catch (E) {
+xmlhttp = false;
+}
+}
+if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
+  xmlhttp = new XMLHttpRequest();
+  }
+  return xmlhttp;
+  }
+
 function LimpiarCampos(){
   document.getElementById("login").value="";
   document.getElementById("pass").value="";
